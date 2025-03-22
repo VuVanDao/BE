46950f696c38004a8fd6088bc4082161e24701c6
Van Dao <dao28901@gmail.com>
2025-03-22 23:45:03 +0700

    setup file backend

--- src/server.js
+++ src/server.js
@@ -1,13 +1,36 @@
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+
 import express from "express";
-// const express = require("express");
+import { mapOrder } from "~/utils/sorts.js";
+
 const app = express();
-const hostName = "localhost";
+
+const hostname = "localhost";
 const port = 8080;
+
 app.get("/", (req, res) => {
-  res.send("<h1>Hello world!!!</h1>");
-});
-app.listen(port, hostName, () => {
+  // Test Absolute import mapOrder
   console.log(
-    `sever ${hostName} start in port ${port}. link http://${hostName}:${port}`
+    mapOrder(
+      [
+        { id: "id-1", name: "One" },
+        { id: "id-2", name: "Two" },
+        { id: "id-3", name: "Three" },
+        { id: "id-4", name: "Four" },
+        { id: "id-5", name: "Five" },
+      ],
+      ["id-5", "id-4", "id-2", "id-3", "id-1"],
+      "id"
+    )
   );
+  res.end("<h1>Hello World!</h1><hr>");
+});
+
+app.listen(port, hostname, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Hello Trung Quan Dev, I am running at ${hostname}:${port}/`);
 });
