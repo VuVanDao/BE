45bd3babe72e8d8de97554b161c989c978e1ca00
Van Dao <dao28901@gmail.com>
2025-03-24 15:14:35 +0700

    tao router

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,3 +3,14 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+import express from "express";
+import { StatusCodes } from "http-status-codes";
+import { boardRouter } from "./boardRoutes";
+const Router = express.Router();
+Router.get("/status", (req, res) => {
+  res.status(StatusCodes.OK).json({ Message: "API v1  are ready to use" });
+});
+
+// boards api
+Router.use("/boards", boardRouter);
+export const APIs_V1 = Router;
