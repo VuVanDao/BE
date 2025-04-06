3f7320404f7b90114b8c178c05b22585f97b6920
Van Dao <dao28901@gmail.com>
2025-04-06 19:20:51 +0700

    update cors not allow localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,9 @@
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
 //nhung domain dc phep truy cap toi server
-export const WHITELIST_DOMAINS = ["http://localhost:5173"];
+export const WHITELIST_DOMAINS = [
+  // "http://localhost:5173"
+];
 export const board_types = {
   public: "public",
   private: "private",
